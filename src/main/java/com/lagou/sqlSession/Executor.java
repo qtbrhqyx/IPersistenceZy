ef37902ed55f48dcbf943d591c5bc37aacc4a9fb
yucz <qiqingchong@163.com>
2020-01-03 18:50:29 +0800

    Initial commit

---
+++ src/main/java/com/lagou/sqlSession/Executor.java
@@ -0,0 +1,19 @@
+package com.lagou.sqlSession;
+
+import com.lagou.pojo.Configuration;
+import com.lagou.pojo.MappedStatement;
+
+import java.util.List;
+
+public interface Executor {
+
+    public <E> List<E> query(Configuration configuration,MappedStatement mappedStatement,Object... params) throws Exception;
+
+    void insert(Configuration configuration,MappedStatement mappedStatement,Object params) throws Exception;
+
+    void update(Configuration configuration,MappedStatement mappedStatement,Object params) throws Exception;
+
+    void delete(Configuration configuration,MappedStatement mappedStatement,Object params) throws Exception;
+
+
+}
